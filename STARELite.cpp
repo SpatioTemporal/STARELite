2170d0a703f2ff96a621b58648bdd643e6601d89
TonHai1111 <tonthatdaihai@gmail.com>
2022-03-03 10:38:03 -0600

    Added decode_float_blob, encode_float_blob, sum_blob_all_element_array, sum_blob_array

--- STARELite.cpp
+++ STARELite.cpp
@@ -252,7 +252,7 @@ static void decode_stareblob(sqlite3_context *context, int argc, sqlite3_value *
     char result_string[str.length()]; 
     strcpy(result_string, str.c_str());
     
-    sqlite3_result_text(context, result_string, 500, SQLITE_TRANSIENT);
+    sqlite3_result_text(context, result_string, 500, SQLITE_TRANSIENT);//Why 500? should it be str.length()?
 }
 
 void removeChar(char *str, char garbage) {
@@ -300,7 +300,148 @@ static void encode_stareblob(sqlite3_context *context, int argc, sqlite3_value *
     sqlite3_result_blob64(context, &array, size, SQLITE_TRANSIENT);      
 }
 
+static void encode_float_blob(sqlite3_context *context, int argc, sqlite3_value **argv) {
+    
+    if (sqlite3_value_type(argv[0]) != SQLITE_TEXT) {
+	  sqlite3_result_null(context);
+	  return;
+    }
+    
+    char *string = reinterpret_cast<char*>(const_cast<unsigned char*>(sqlite3_value_text(argv[0])));
+    removeChar(string, '[');
+    removeChar(string, ']');
+    removeChar(string, ' ');
 
+    char * token = strtok(string, ",");
+    char* pEnd;
+    std::vector<double> dbls;
+    while( token != NULL ) {
+        dbls.push_back(strtoll(token, &pEnd, 10));
+        token = strtok(NULL, "\n");
+    }
+    
+    // Convert vector to array
+    int size = dbls.size() * sizeof(dbls[0]);
+    double array[dbls.size()];
+    int i = 0;
+    for (double val: dbls){
+        array[i] = val;
+        i ++;
+    }
+    sqlite3_result_blob64(context, &array, size, SQLITE_TRANSIENT);      
+}
+
+std::vector<double> blob2doubles(unsigned char *p_blob, int n_bytes) {
+    std::vector<double> dbls;    
+    int size = sizeof(double);
+    for (int i=0; i<n_bytes; i+=size) {        
+        dbls.push_back(gaiaImport64(p_blob + i, 1, 1));
+    }
+    return dbls;
+}
+
+static void decode_float_blob(sqlite3_context *context, int argc, sqlite3_value **argv) {
+    unsigned char *p_blob;
+    int n_bytes;
+    
+    if (sqlite3_value_type(argv[0]) != SQLITE_BLOB) {
+	  sqlite3_result_null(context);
+	  return;
+    }
+        
+    p_blob = (unsigned char *) sqlite3_value_blob(argv[0]);
+    n_bytes = sqlite3_value_bytes(argv[0]);
+    std::vector<double> dbls = blob2doubles(p_blob, n_bytes);
+    
+    std::string str;
+    str += "[";
+    for (double value: dbls) {
+        str += std::to_string(value);        
+        str += ", ";
+    }
+    str += "]";
+        
+    char result_string[str.length()]; 
+    strcpy(result_string, str.c_str());
+    
+    sqlite3_result_text(context, result_string, str.length(), SQLITE_TRANSIENT);
+}
+
+/*https://www.sqlite.org/appfunc.html
+Example: 
+typedef struct CountCtx CountCtx;
+struct CountCtx {
+  i64 n;
+};
+static void countStep(sqlite3_context *context, int argc, sqlite3_value **argv){
+  CountCtx *p;
+  p = sqlite3_aggregate_context(context, sizeof(*p));
+  if( (argc==0 || SQLITE_NULL!=sqlite3_value_type(argv[0])) && p ){
+    p->n++;
+  }
+}   
+static void countFinalize(sqlite3_context *context){
+  CountCtx *p;
+  p = sqlite3_aggregate_context(context, 0);
+  sqlite3_result_int64(context, p ? p->n : 0);
+}
+*/
+/* An Aggregate function that is used to sum up all ellements of all BLOB array tuples */
+typedef struct Sum_BLOB_Array Sum_BLOB_Array; 
+struct Sum_BLOB_Array {
+    double sum;
+}
+static void sum_blob_array_Step(sqlite3_context *context, int argc, sqlite3_value **argv){
+    Sum_BLOB_Array *p;
+    p = sqlite3_aggregate_context(context, sizeof(*p));
+
+    unsigned char *p_blob;
+    int n_bytes;
+    
+    if (sqlite3_value_type(argv[0]) != SQLITE_BLOB) {
+	  sqlite3_result_null(context);
+	  return;
+    }
+        
+    p_blob = (unsigned char *) sqlite3_value_blob(argv[0]);
+    n_bytes = sqlite3_value_bytes(argv[0]);
+    std::vector<double> dbls = blob2doubles(p_blob, n_bytes);
+
+    for (double value: dbls) {
+        p->sum += value;
+    }
+}
+
+static void sum_blob_array_Final(sqlite3_context *context){
+    Sum_BLOB_Array *p;
+    p = sqlite3_aggregate_context(context, sizeof(*p));
+    std:string str_sum = std::to_string(p->sum);
+    sqlite3_result_text(context, str_sum, str_sum.length(), SQLITE_TRANSIENT);
+    //sqlite3_result_double(context, p->sum); if a double is expected
+}
+
+static void sum_blob_all_element_array(sqlite3_context *context, int argc, sqlite3_value **argv){
+    unsigned char *p_blob;
+    int n_bytes;
+    
+    if (sqlite3_value_type(argv[0]) != SQLITE_BLOB) {
+	  sqlite3_result_null(context);
+	  return;
+    }
+        
+    p_blob = (unsigned char *) sqlite3_value_blob(argv[0]);
+    n_bytes = sqlite3_value_bytes(argv[0]);
+    std::vector<double> dbls = blob2doubles(p_blob, n_bytes);
+    
+    double dbl_sum = 0.0;
+    for (double value: dbls) {
+        dbl_sum += value;
+    }
+    std::string str_sum = std::to_string(dbl_sum);
+    
+    sqlite3_result_text(context, str_sum, str_sum.length(), SQLITE_TRANSIENT);
+    //sqlite3_result_double(context, dbl_sum);//If we want to return a double instead of string
+}
 
 /*
 ** Implementation of stare_intersects() with helper functions brute_intersects and sorted_intersects().
@@ -447,10 +588,51 @@ extern "C" {
         rc = sqlite3_create_function(db, "encode_stareblob", 1,
                                      SQLITE_UTF8|SQLITE_INNOCUOUS|SQLITE_DETERMINISTIC,
                                      0, encode_stareblob, 0, 0);
+        rc = sqlite3_create_function(db, "decode_float_blob", 1,
+                                     SQLITE_UTF8|SQLITE_INNOCUOUS|SQLITE_DETERMINISTIC,
+                                     0, decode_float_blob, 0, 0);
+        rc = sqlite3_create_function(db, "encode_float_blob", 1,
+                                     SQLITE_UTF8|SQLITE_INNOCUOUS|SQLITE_DETERMINISTIC,
+                                     0, encode_float_blob, 0, 0);
+        rc = sqlite3_create_function(db, "sum_blob_all_element_array", 1,
+                                     SQLITE_UTF8|SQLITE_INNOCUOUS|SQLITE_DETERMINISTIC,
+                                     0, sum_blob_all_element_array, 0, 0);
         rc = sqlite3_create_function(db, "stare_intersects", 2,
                                      SQLITE_UTF8|SQLITE_INNOCUOUS|SQLITE_DETERMINISTIC,
                                      0, stare_intersects, 0, 0);
-                
+        /* Aggregate function */
+        rc = sqlite3_create_function(db, "sum_blob_array", 1,
+                                     SQLITE_UTF8|SQLITE_INNOCUOUS|SQLITE_DETERMINISTIC,
+                                     0, 0, sum_blob_array_Step, sum_blob_array_Final);
+/*
+https://www.sqlite.org/capi3.html:
+   typedef struct sqlite3_value sqlite3_value;
+   int sqlite3_create_function(
+     sqlite3 *,
+     const char *zFunctionName,
+     int nArg,
+     int eTextRep,
+     void*,
+     void (*xFunc)(sqlite3_context*,int,sqlite3_value**),
+     void (*xStep)(sqlite3_context*,int,sqlite3_value**),
+     void (*xFinal)(sqlite3_context*)
+   );
+   int sqlite3_create_function16(
+     sqlite3*,
+     const void *zFunctionName,
+     int nArg,
+     int eTextRep,
+     void*,
+     void (*xFunc)(sqlite3_context*,int,sqlite3_value**),
+     void (*xStep)(sqlite3_context*,int,sqlite3_value**),
+     void (*xFinal)(sqlite3_context*)
+   );
+   #define SQLITE_UTF8     1
+   #define SQLITE_UTF16    2
+   #define SQLITE_UTF16BE  3
+   #define SQLITE_UTF16LE  4
+   #define SQLITE_ANY      5
+*/                
         return rc;
     }
 }
